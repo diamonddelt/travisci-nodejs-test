636476af7e659bdb1d1bbb52c36e1adc86714a28
diamonddelt <rrrasti@yahoo.com>
2019-02-02 13:05:39 -0600

    Initial commit to test Travis CI on NodeJS

---
+++ test.js
@@ -0,0 +1,14 @@
+// testing Travis CI on a nodejs app
+// the package.json defines the test behavior (which is to run JSLint; a javascript linter for code quality)
+
+const host = '127.0.0.1';
+const port = 2333;
+
+let http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Diamond Delt\n');
+}).listen(port, host);
+
+console.log(`NodeJS server running at ${host}:${port}...`);
